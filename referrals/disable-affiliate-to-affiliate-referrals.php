520c38a5f112a6ba9de6cc5db022921df95a5dd0
Ginger Coolidge <gspice@users.noreply.github.com>
2023-01-10 13:13:12 -0800

    Update disable-affiliate-to-affiliate-referrals.php

--- referrals/disable-affiliate-to-affiliate-referrals.php
+++ referrals/disable-affiliate-to-affiliate-referrals.php
@@ -5,17 +5,17 @@
  * Description: Prevents affiliates from earning a commission if the purchase was made by another logged-in affiliate
  * Author: Andrew Munro
  * Author URI: http://affiliatewp.com
- * Version: 1.0
+ * Version: 2.0
  */
+function affwp_custom_disable_affiliate_to_affiliate_referrals( $referral_amount, $affiliate_id, $amount, $reference, $product_id, $context ) {
+	$user = wp_get_current_user();
 
-function affwp_custom_disable_affiliate_to_affiliate_referrals( $referral_amount, $affiliate_id, $amount, $reference, $product_id ) {
+	// affwp_is_affiliate() will check if the currently logged in user is an affiliate.
+	// if they are an affiliate, set the referral amount to 0.00
+	if ( affwp_is_affiliate( $user->ID ) ) {
+		$referral_amount = 0.00;
+	}
 
-  // affwp_is_affiliate() will check if the currently logged in user is an affiliate.
-  // if they are an affiliate, set the referral amount to 0.00
-  if ( affwp_is_affiliate() ) {
-    $referral_amount = 0.00;
-  }
-
-  return $referral_amount;
+	return $referral_amount;
 }
-add_filter( 'affwp_calc_referral_amount', 'affwp_custom_disable_affiliate_to_affiliate_referrals', 10, 5 );
+add_filter( 'affwp_calc_referral_amount', 'affwp_custom_disable_affiliate_to_affiliate_referrals', 10, 6 );
